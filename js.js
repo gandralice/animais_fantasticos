21ef0405e6a362f8453c8dfc451436de1a5361d6
ALICE <gandralice@gmail.com>
2025-06-26 10:59:40 -0300

    a0312

--- js.js
+++ js.js
@@ -19,11 +19,54 @@ function init_tabnav() {
 init_tabnav();
 
 //0310
-const accordion_list = document.querySelectorAll(".js-accordion dt");
-function active_accordion() {
-  this.classList.toggle("ativo");
-  this.nextElementSibling.classList.toggle("ativo");
+function init_accordion() {
+  const accordion_list = document.querySelectorAll(".js-accordion dt");
+  if (accordion_list.length) {
+    function active_accordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+    accordion_list.forEach((item) => {
+      item.addEventListener("click", active_accordion);
+    });
+  }
 }
-accordion_list.forEach((item) => {
-  item.addEventListener("click", active_accordion);
-});
+init_accordion();
+
+//0311
+function init_scroll() {
+  const links_int = document.querySelectorAll('a[href^= "#"]');
+  function scroll_to(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    console.log(section);
+    section.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+
+  links_int.forEach((link) => {
+    link.addEventListener("click", scroll_to);
+  });
+}
+init_scroll();
+
+//0312
+function init_ascroll() {
+  const sections = document.querySelectorAll(".grid-section");
+  if (sections.length) {
+    const window_h = window.innerHeight * 0.7;
+    function anima_scroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - window_h;
+        if (sectionTop < 0) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    anima_scroll();
+    window.addEventListener("scroll", anima_scroll);
+  }
+}
+init_ascroll();
